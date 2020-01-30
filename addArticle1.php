ac7a4ebd471b41a413991bb1d19b32856c874676
MGuillard87 <maty.guillard@le-campus-numerique.fr>
2020-01-30 09:27:07 +0100

    Ajout de l'exercice catalogue

--- addArticle1.php
+++ addArticle1.php
@@ -1,6 +1,55 @@
 <?php
-session_start();
+// fonction 'include' pour faire appel au fonction d'affiche d'article
+include ("catalogue_fonction.php");
+
+// Variable 'erreurs' pour contrôler les conditions lorsqu'il y a des erreurs
+$erreurs = true;
+
+// variable "nom" pour mettre en place la première condition vérifiant si $_POST existe
+$nom="";
+
+// variable erreur pour chaque champ du formulaire qui affiche les erreurs
+$errnom="";
+$errimg="";
+$errprix="";
+
+try {
+
+    if (isset($_POST['nom_article'])) {
+        $nom = $_POST['nom_article'];
+// vérification du champ prix à effectuer avec  la fonction int_nemric(), puis voir si la valeur donner est supérieure à 0
+        if (!intval($_POST['prix_article']) > 0){
+            $errprix = "Vous n'avez pas entré de prix";
+                      $erreurs = false;
+        }
+        else{
+            //Test 1:  Tester si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+            if (isset($_FILES['photo_article']) AND $_FILES['photo_article']['error'] == 0) {
+                // Testons si le fichier n'est pas trop gros
+                if ($_FILES['photo_article']['size'] <= 1000000) {
+                    // Test 2: Tester si l'extension est autorisée: interdire à tout prix que les gens puissent envoyer des fichiers PHP, sinon ils pourraient exécuter des scripts sur notre serveur
+                    $infosfichier = pathinfo($_FILES['photo_article']['name']);
+                    $extension_upload = $infosfichier['extension'];
+                    $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+                    if (in_array($extension_upload, $extensions_autorisees)) {
+                        // Si extension autorisée: on peut valider le fichier et le stocker définitivement
+                        move_uploaded_file($_FILES['photo_article']['tmp_name'], 'images/' . basename($_FILES['photo_article']['name']));
+                        echo "Votre article a bien été envoyé !";
+                    } else {
+                        // Si l'extension n'est pas autorisée: on ne valide pas le fichier et on envoie le client vers une page d'erreur
+                        $errimg =  "L'extension du fichier n'est pas pris en compte par notre site";
+                        $erreurs = false;
+                    }
+                }
+            }
+        }
+    }
+} catch(Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,20 +60,29 @@ session_start();
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-12">
+
+<?php if ($erreurs==false || !isset($_POST['nom_article'])) { ?>
+    <div class="container-fluid">
+      <div class="row">
+         <div class="col-sm-12">
             <h1>Création d'un article</h1>
         </div>
-
-        <form action="displayarticle1.php" method="post" enctype="multipart/form-data">
-            <p><label>l'image de l'article : <input type="file" name="photo_article" required /></label></p>
-            <p><label>Le nom de l'article : <input type="text" name="nom_article" required placeholder="Nom de l'article"/></label></p>
-            <p><label>Le prix de l'article : <input type="text" name="prix_article" required placeholder="Prix en Euros"/></label></p>
+        <form action="addArticle1.php" method="post" enctype="multipart/form-data">
+            <p><label>l'image de l'article : <input type="file" name="photo_article" required /></label><?=$errimg ?></p>
+            <p><label>Le nom de l'article : <input type="text" name="nom_article" required placeholder="Nom de l'article" value="<?=$nom ?>"/></label></p>
+            <p><label>Le prix de l'article : <input type="text" name="prix_article" required placeholder="Prix en Euros"/></label><?=$errprix ?></p>
             <p><input type="submit" value="Envoyer l'article"></p>
         </form>
-
+      </div>
     </div>
-</div>
-</body>
-</html>
+    <?php }
+    else {
+
+        afficheArt($_POST['nom_article'], $_POST['prix_article'], basename($_FILES['photo_article']['name']));?>
+        <div class="row">
+        <p>Pour ajouter un nouvel article <a href="addArticle1.php">cliquez ici</a></p>
+    </div>
+
+
+  <?php  } ?>
+
--- catalogue_fonction.php
+++ catalogue_fonction.php
@@ -4,22 +4,28 @@ $liste_articles = [
     [ "image"=>"images/pull_cerf.jpg", "name"=>"Pull", "price"=>5],
     ["image"=> "images/nounours.jpg", "name"=>"Nounours", "price"=>1500]
 ];
-// création de la fonction pour afficher les articles
+// création de la fonction pour afficher tous les articles
 function afficheArticle($catalogue_articles){
     foreach ($catalogue_articles as $article){
-     ?>
+        afficheart ( $article["image"], $article["name"], $article["price"]);
+    }
+}
+
+//fonction qui affiche 1 article
+function afficheArt($nom, $prix, $img){
+        ?>
         <div class="row" >
             <div class="col">
-                <img src="<?php  echo $article["image"]; ?>" width="300" class="rounded corners img-fluid" alt="article à acheter">
+                <img src="images/<?= $img ?>" width="300" class="rounded corners img-fluid" alt="article à acheter">
             </div>
 
             <div class="col">
-                <h2><?php echo $article["name"];?></h2>
+                <h2><?= $nom?></h2>
             </div>
 
             <div class="col">
-                <h2><?php echo $article["price"]." euros" ; ?> </h2>
+                <h2><?= $prix." euros" ; ?> </h2>
             </div>
         </div>
-    <?php }
+    <?php
 } ?>
