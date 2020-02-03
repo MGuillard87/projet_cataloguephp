ac3b409ee9d1159b73043a2eb30322d4c4106978
MGuillard87 <maty.guillard@le-campus-numerique.fr>
2020-02-03 13:25:27 +0100

    1er commit phpstorm

--- panier.php
+++ panier.php
@@ -1,6 +1,9 @@
 <?php
+// inclure les fonctions et les données via un fichier pour les utiliser dans ce fichier
+include ('catalogue_fonction.php');
 
-
+// Toujours vérifier que les informatons utilisées existent: var_dump très utile dans ce cas
+//var_dump($liste_articles)
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -19,38 +22,50 @@
         </div>
     </div>
     <?php
+
     // affichage des variables nommées plus haut ?>
     <?php      // création de la foreach for pour afficher chaque article avec sa photo, son prix et son nom
-//   try {
-//    if ( !empty($_POST['case'])){
-//
-    if (isset($_POST["case"])) { ?>
-    // La case est cochée: afficher les articles
-      <div class="row panier" >
-            <div class="col">
-                <img src="<?php  echo $_POST["case['image']"]; ?>" width="300" class="rounded corners img-fluid" alt="article à acheter">
-            </div>
+    try {
+        if (!empty($_POST)) {
+            //Toujours vérifier que les informatons utilisées existent et contiennent des données
+//            var_dump($_POST);
+    // Si $_POST contient une/des données: la case est cochée et on peut afficher les articles
 
-            <div class="col">
-                <h2><?php echo $_POST['$article["name"]'];?></h2>
-            </div>
-            <div class="col">
-                <h2><?php echo $_POST['$article["price"]']." euros" ; ?> </h2>
-            </div>
-    </div>
-</div>
-<?php }
-    else {
-    // La case est décochée
+    // On va boucler dans la superVariable $_POST pour aller chercher l'information/donnée qui nous intéresse
+
+            foreach ($_POST as $key => $selective){
+                // on enregistre dans une variable les informations récupérées
+                $article=$liste_articles[$key] ?>
+                <div class="row panier">
+                    <div class="col">
+                        <img src="<?php echo $article['image']; ?>" width="300" class="rounded corners img-fluid"
+                             alt="article à acheter">
+                    </div>
+
+                    <div class="col">
+                        <h2><?php echo $article['name']; ;?></h2>
+                    </div>
+
+                    <div class="col">
+                        <h2><?php echo $article['price'] . " euros"; ?> </h2>
+                    </div>
+                </div>
+<?php
+
+            }
+        }
+        else {
+
     }
 
+    } catch(Exception $e) {
+        // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+    }
+    ?>
 
 
 
-//catch(Exception $e){
-//        die('Erreur : '.$e->getMessage());
-//}
-?>
 
     </body>
 </html>
